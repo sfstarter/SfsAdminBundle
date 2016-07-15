586915049cb8e81540a6e53ace7b34d6948c5d2d
Ramine Agoune <ramine.agoune@solidlynx.com>
2016-07-15 16:17:36 +0200

    Remove of FOSUser requirement

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -8,20 +8,43 @@
 
 namespace Sfs\AdminBundle\Controller;
 
-use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Exception\RuntimeException;
+use Symfony\Component\HttpFoundation\Request;
 
-/**
- * {@inheritDoc}
- */
-class SecurityController extends BaseController
+
+class SecurityController extends Controller
 {
-    /**
-     * {@inheritDoc}
-     */
-	public function renderLogin(array $data)
-	{
-		$template = sprintf('SfsAdminBundle:Security:login.html.twig');
+	/**
+	 * @param Request $request
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function loginAction(Request $request) {
+		$authenticationUtils = $this->get('security.authentication_utils');
 
-		return $this->container->get('templating')->renderResponse($template, $data);
+		// get the login error if there is one
+		$error = $authenticationUtils->getLastAuthenticationError();
+
+		// last username entered by the user
+		$lastUsername = $authenticationUtils->getLastUsername();
+
+		// Csrf token generation
+		$csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
+
+		return $this->render('SfsAdminBundle:Security:login.html.twig', array(
+			'last_username' => $lastUsername,
+			'error'         => $error,
+			'csrf_token' 	=> $csrfToken
+		));
+	}
+
+
+	/**
+	 * Symfony is supposed to intercept the logout call, only a misconfiguration would lead to go into the action
+	 *
+	 * @throws RuntimeException
+	 */
+	public function logoutAction() {
+		throw new \RuntimeException('Configure the logout entry in your firewall.');
 	}
 }
